573aa5c7fe1bed5d3cfbbdc636affaf738592672
Alejandro Lago Pereira <alejandro.lago@proton.me>
2026-06-02 15:22:52 +0200

    FIX: two returns in the Interface repository was wrong, now returns nulleables in case of null

--- Services/IExerciseApiService.cs
+++ Services/IExerciseApiService.cs
@@ -5,6 +5,6 @@ namespace TfgApi.Services;
 public interface IExerciseApiService
 {
     Task<List<ExerciseItemDTO>> SearchExerciseAsync(string? name = null, string? muscle = null, int page = 1, int pageSize = 30);
-    Task<ExerciseItemDTO> GetExerciseByIdAsync(string exerciseId);
-    Task<ExerciseItemDTO> GetRandomExerciseAsync();
+    Task<ExerciseItemDTO?> GetExerciseByIdAsync(string exerciseId);
+    Task<ExerciseItemDTO?> GetRandomExerciseAsync();
 }
